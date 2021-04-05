10c25693dcba6ea479b90d23185797b6f80a0880
Christian Zagorski <christianz@hey.com>
2021-04-04 20:17:22 -0700

    Initial Commit

---
+++ ThereNoLogin/ThereNoLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThereNoLoginApp.swift
+//  ThereNoLogin
+//
+//  Created by Christian Zagorski on 4/4/21.
+//
+
+import SwiftUI
+
+@main
+struct ThereNoLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
