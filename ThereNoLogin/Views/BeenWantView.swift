5d9f410dfee4d7323d2a9d56916f611fd51e1805
Christian Zagorski <christianz@hey.com>
2021-04-04 21:41:03 -0700

    Start BeenThereView UI code framework

--- ThereNoLogin/Views/BeenWantView.swift
+++ ThereNoLogin/Views/BeenWantView.swift
@@ -9,9 +9,45 @@ import SwiftUI
 
 struct BeenWantView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        
+        TabView {
+            
+            GeometryReader { geo in
+                
+                NavigationView {
+                    
+                    ScrollView {
+                        
+                        LazyVStack {
+                            
+                            LazyHStack {
+                                
+                                if geo.size.width < 400 {
+                                    // If screen width is small make two tiles horizontally
+                                }
+                                
+                                else {
+                                    // If screen width is large make four tiles horizontally
+                                }
+                                
+                            } // End LazyHStack
+                            
+                        } // End LazyVStack
+                        
+                    } // End ScrollView
+                    
+                } // End Navigation View
+                
+            } // End GeometryReader
+   
+        } // End TabView
+        
+        
+        
+        
+    } // End body
+    
+} // End BeenWantView structure
 
 struct BeenWantView_Previews: PreviewProvider {
     static var previews: some View {
